1bdee6455d5b55aff7da45f05b9698d368c2f111
Yan Wei <asurada.gen@gmail.com>
2015-11-06 18:30:23 +0900

    images added

--- caranal/common/models/m-product.js
+++ caranal/common/models/m-product.js
@@ -1,19 +1,36 @@
 module.exports = function(MProduct) {
 	MProduct.insertProduct = function(name,detail,price,cb) {
 		console.log(name +';'+ detail+';'+price);
-     	MProduct.create({name: name ,detail:detail,price:price ,regdt:Date.now(),updt:Date.now()},cb);
+    MProduct.create({name: name ,detail:detail,price:price ,regdt:Date.now(),updt:Date.now()},cb);
 	};
 
+  MProduct.updateProduct = function(name,detail,price,cb) {
+    console.log(name +';'+ detail+';'+price);
+    MProduct.create({name: name ,detail:detail,price:price, regdt:Date.now(), updt:Date.now()} ,cb);
+  };
+
+  
+
+
+
 	MProduct.remoteMethod(
-        'insertProduct',
-        {
-          http: {path: '/insertProduct', verb: 'post'},
-          accepts: [{arg: 'name', type: 'String', http: { source: 'query' } },
-                    {arg: 'detail', type: 'String', http: { source: 'query' }},
-                    {arg: 'price', type: 'Number', http: { source: 'query' }}],
-          returns: {arg: 'answer', type: 'string'}
-        }
-    );
+      'insertProduct',
+      {
+        http: {path: '/insertProduct', verb: 'post'},
+        accepts: [{arg: 'name', type: 'String', http: { source: 'query' } },
+                  {arg: 'detail', type: 'String', http: { source: 'query' }},
+                  {arg: 'price', type: 'Number', http: { source: 'query' }}],
+        returns: {arg: 'answer', type: 'string'}
+      }
+  );
+
+
+
+
+
+
+
+
 
 
 
