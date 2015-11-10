176122b55f5112e7b6d3e049036721f5e0c77c0e
Yan Wei <asurada.gen@gmail.com>
2015-11-10 18:43:39 +0900

    2015-11-10

--- caranal/common/models/mproduct.js
+++ caranal/common/models/mproduct.js
@@ -1,3 +1,22 @@
 module.exports = function(Mproduct) {
+	Mproduct.createProduct =  function(id,product_name,detail,cb) 
+		Mproduct.create({ID:id, 
+					  NAME:product_name, 
+					  DETAIL:detail,
+					  UPDATE_DATE:Date.now(),
+					  REGDT_DATE:Date.now(),
+					  IS_DELETE:'0'},cb);
+	};
+	
 
+	Mproduct.remoteMethod (
+        'createProduct',
+        {
+          http: {path: '/createProduct', verb: 'post'},
+          accepts: [{arg: 'id', type: 'number', http: { source: 'query' } },
+          			{arg: 'product_name', type: 'string', http: { source: 'query' } },
+          			{arg: 'detail', type: 'string', http: { source: 'query' }}],
+          returns: {arg: 'answer', type: 'string'}
+        }
+    );
 };
